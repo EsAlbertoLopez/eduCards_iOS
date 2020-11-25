c794e2b69f51e2d137fa27c532ff72c0ea80f258
RANO <rano.edu.itesm@gmail.com>
2020-11-24 20:31:11 -0600

    Initial Commit

---
+++ eduCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eduCards
+//
+//  Created by Alberto López Reyes on 24/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
