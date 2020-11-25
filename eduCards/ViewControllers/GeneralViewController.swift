2327c895f4499b440fc08bf707416614f743f28a
RANO <rano.edu.itesm@gmail.com>
2020-11-25 10:10:38 -0600

    Alpha 4.1: Forgot to push these files in the last update.

--- eduCards/ViewControllers/GeneralViewController.swift
+++ eduCards/ViewControllers/GeneralViewController.swift
@@ -1,19 +1,20 @@
-//
-//  GeneralViewController.swift
-//  eduCards
-//
-//  Created by Alberto López Reyes on 25/11/20.
-//
-
 import UIKit
 
 class GeneralViewController: UIViewController {
     
     @IBOutlet weak var tvSubtitle: UILabel!
+    @IBOutlet weak var tbCourses: UITableView!
+    
+    private let arrTestCells = ["Test Course"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Courses"
+        setTabBarItemColor()
+    }
+    
+    func setTabBarItemColor() {
+        self.tabBarController?.tabBar.tintColor = UIColor(red: 0.596, green: 0.043, blue: 0.31, alpha: 1)
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -35,5 +36,38 @@ class GeneralViewController: UIViewController {
         } else {
             tvSubtitle?.textAlignment = .left
         }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setTabBarItemColor()
+        if UIDevice.current.orientation.isLandscape {
+            switch UIDevice.current.userInterfaceIdiom {
+            case .phone:
+                tvSubtitle?.textAlignment = .center
+                break
+            case .pad:
+                tvSubtitle?.textAlignment = .left
+                break
+            case .unspecified: break
+            case .tv: break
+            case .carPlay: break
+            case .mac: break
+            @unknown default: break
+            }
+        } else {
+            tvSubtitle?.textAlignment = .left
+        }
+    }
+}
+
+extension GeneralViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrTestCells.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath)
+        cell.textLabel?.text = arrTestCells[indexPath.row]
+        return cell
     }
 }
